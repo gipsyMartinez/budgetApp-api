c333eb94bbedc56b0c9e6e25e9c2f3a6fbccbd22
Gipsy Martinez <ext_gdmartinez@falabella.cl>
2017-12-26 12:17:32 -0300

    User in Budget - Added user_id to budgets table - Added relationships in model - Added test
    This close #6

--- spec/factories/budgets.rb
+++ spec/factories/budgets.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     expense_id { Expense.last&.id || FactoryBot.create(:expense).id }
     month_id { Month.last&.id || FactoryBot.create(:month).id }
     amount { Faker::Number.decimal(5, 2) }
+    user_id { User.last&.id || FactoryBot.create(:user).id }
   end
 end
--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Budget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it { should belong_to(:month) }
+    it { should belong_to(:expense) }
+    it { should belong_to(:user) }
+  end
 end
