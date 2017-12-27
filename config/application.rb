00428430866259616ea95ae874fe79e61651ab9a
Gipsy Martinez <ext_gdmartinez@falabella.cl>
2017-12-27 12:31:59 -0300

    Added patch, put, delete to rack cors
    This closes #12

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module BudgetAppApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :patch, :put, :options]
       end
     end
     config.api_only = true
