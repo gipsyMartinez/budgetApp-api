687f069045d2ca820a817bb707694c0900b8cbcb
Gipsy Martinez <ext_gdmartinez@falabella.cl>
2018-01-03 20:36:18 -0300

    Month Removed
    - Month relation and field removed
    
    This close  #13

--- spec/factories/months.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :month do
-    name { FFaker::Lorem.word }
-  end
-end
--- spec/models/month_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Month, type: :model do
-  describe 'Validations' do
-    it { should validate_presence_of :name }
-  end
-end
